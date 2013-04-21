4c24275f33f9a508fcbc3bee1dc42b15fc18109c
Camilo Aguilar <camilo.aguilar@gmail.com>
2013-04-21 05:28:33 -0400

    Attempting to compile StandaloneAmbManager

--- scripts/alma.sh
+++ scripts/alma.sh
@@ -1,5 +1,8 @@
 # User specific aliases and functions
 source /alma/ACS-10.2/ACSSW/config/.acs/.bash_profile.acs
 
+#ACE_ROOT is being set by .bash_profile.acs already
+#export ACE_ROOT=/alma/ACS-10.2/TAO/ACE_wrappers/build/linux
+export TAO_ROOT=$ACE_ROOT/TAO
 export LD_LIBRARY_PATH=/home/dev/CURRENTLY_MOD/StandaloneAmbManager/lib:$LD_LIBRARY_PATH
-export CPLUS_INCLUDE_PATH=/alma/ACS-10.2/TAO/ACE_wrappers/ace:/alma/ACS-10.2/TAO/ACE_wrappers/TAO/tao:/alma/ACS-10.2/ACSSW/include
+export CPLUS_INCLUDE_PATH=$ACE_ROOT/ace:$TAO_ROOT/tao:/alma/ACS-10.2/ACSSW/include:$TAO_ROOT/tao/IORTable:$TAO_ROOT/tao/IFR_Client:$TAO_ROOT/tao/PortableServer:$TAO_ROOT/tao/SmartProxies:$TAO_ROOT/tao/DynamicAny:$TAO_ROOT/tao/DynamicInterface:$TAO_ROOT/tao/Messaging:$TAO_ROOT/tao/Valuetype:$TAO_ROOT/orbsvcs/orbsvcs:$TAO_ROOT/orbsvcs:$TAO_ROOT/orbsvcs/orbsvcs/Log
