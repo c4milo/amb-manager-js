ccfb1805d85cebddaae8861bb1176a1f39460687
Camilo Aguilar <camilo.aguilar@gmail.com>
2013-04-21 17:37:30 -0400

    Fixes ACE/TAO compilation errors

--- binding.gyp
+++ binding.gyp
@@ -2,9 +2,13 @@
   'targets': [
     {
       'target_name': 'amb-manager',
+      'defines': ['_POSIX_THREADS', '_POSIX_THREAD_SAFE_FUNCTIONS', '_REENTRANT', 'ACE_HAS_AIO_CALLS', 'DEBUG', 'CCS_LIGHT', 'POSN_DUMP'],
       'include_dirs': [
         '/home/dev/CURRENTLY_MOD/StandaloneAmbManager/include'
       ],
+      'cflags': [ '-Wall', '-Wno-unused-parameter'],
+      'cflags_cc': [ '-fPIC', '-fcheck-new', '-fexceptions', '-frtti', ''],
+      #'ldflags': [ '-pthread', '-rdynamic' ],
       'sources': [
         'src/bindings.cc',
         'src/amb-manager.cc'
--- scripts/alma.sh
+++ scripts/alma.sh
@@ -1,4 +1,3 @@
-#Copy this file to /etc/profile.d/alma.sh and source it
 # User specific aliases and functions
 source /alma/ACS-10.2/ACSSW/config/.acs/.bash_profile.acs
 
@@ -6,4 +5,5 @@ source /alma/ACS-10.2/ACSSW/config/.acs/.bash_profile.acs
 #export ACE_ROOT=/alma/ACS-10.2/TAO/ACE_wrappers/build/linux
 export TAO_ROOT=$ACE_ROOT/TAO
 export LD_LIBRARY_PATH=/home/dev/CURRENTLY_MOD/StandaloneAmbManager/lib:$LD_LIBRARY_PATH
-export CPLUS_INCLUDE_PATH=$ACE_ROOT/ace:$TAO_ROOT/tao:/alma/ACS-10.2/ACSSW/include:$TAO_ROOT/tao/IORTable:$TAO_ROOT/tao/IFR_Client:$TAO_ROOT/tao/PortableServer:$TAO_ROOT/tao/SmartProxies:$TAO_ROOT/tao/DynamicAny:$TAO_ROOT/tao/DynamicInterface:$TAO_ROOT/tao/Messaging:$TAO_ROOT/tao/Valuetype:$TAO_ROOT/orbsvcs/orbsvcs:$TAO_ROOT/orbsvcs:$TAO_ROOT/orbsvcs/orbsvcs/Log
+export CPLUS_INCLUDE_PATH=$ACE_ROOT:$ACE_ROOT/ace:$TAO_ROOT:$TAO_ROOT/tao:/alma/ACS-10.2/ACSSW/include:$TAO_ROOT/tao/IORTable:$TAO_ROOT/tao/IFR_Client:$TAO_ROOT/tao/PortableServer:$TAO_ROOT/tao/SmartProxies:$TAO_ROOT/tao/DynamicAny:$TAO_ROOT/tao/DynamicInterface:$TAO_ROOT/tao/Messaging:$TAO_ROOT/tao/Valuetype:$TAO_ROOT/orbsvcs/orbsvcs:$TAO_ROOT/orbsvcs:$TAO_ROOT/orbsvcs/orbsvcs/Log:$ACE_ROOT/include:/alma/ACS-10.2/boost/include:/alma/ACS-current/Python/include/python2.6:/usr/X11R6/include/X11:/alma/ACS-10.2/ACSSW/include/cat:/alma/ACS-10.2/ACSSW/include/rtd:/alma/ACS-10.2/ACSSW/include:/alma/ACS-10.2/Python/include/python2.6:/alma/ACS-10.2/tcltk/include:/usr/include:/usr/local/include
+#export C_INCLUDE_PATH=$CPLUS_INCLUDE_PATH
