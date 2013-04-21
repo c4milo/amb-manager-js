f5a26b9c86b5b9463b9959589aa54053951f0ab5
Camilo Aguilar <camilo.aguilar@gmail.com>
2013-04-20 20:32:01 -0400

    Adds profile.d script in order to set up environment variables for ACS

---
+++ scripts/alma.sh
@@ -0,0 +1,4 @@
+# User specific aliases and functions
+source /alma/ACS-10.2/ACSSW/config/.acs/.bash_profile.acs
+
+export LD_LIBRARY_PATH=/home/dev/CURRENTLY_MOD/StandaloneAmbManager/lib:$LD_LIBRARY_PATH
